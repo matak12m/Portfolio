ae0bfc7101a8b25f9fe4e6a6f81fa5d2105a03f7
unknown <C00288248@setu.ie>
2024-04-15 22:44:22 +0100

    project card expand prototype fully functional - clowning around

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,22 +71,45 @@ function copyEmail() {
 }
 
 let currentProject; //stores the current element being expanded
-
+let storedHTML;
 
 function expandProject(projectCard) {
     console.log("expanding project")
-    //expand the card and append text, images
+    //expand the card and the inner HTML of the card
     //change the css to project-card-expanded
     //add event listener for clicks outside the project card - main + the 2 following sections?
     
 
     if (projectCard.includes("GGJ24")) {
-        currentProject = document.getElementById("GGJ24")
+
+
+        currentProject = document.getElementById(projectCard)
+        storedHTML = currentProject.innerHTML;  //stores the HTML to be restored once the project is minimized
         currentProject.className = 'project project-card-expanded'
-        currentProject.innerHTML = "surprise!"  // fill in actual content of the card. remove in minimizeProject()
-        console.log("expanding GGJ24")
+        currentProject.innerHTML = '<a><img id = "img1" src = "assets/img/clowning-around.png"> </a> <section id = "text1"> For Global Game Jam 2024, the theme was: "Make Me Laugh". I came forward with an idea for a puzzle platformer, where you play as a clown in a circus world. You can make a clown joke (honk your nose) near the resident walking mouths to make them laugh, which creates a physical HaHa, which can interact with the environment - pop balloons, break platform, etc. </section><section id = "text2">My role on the team was being the Artist and Creative Lead. <br>Our team ended up winning a "Best use of theme" award at the end of the weekend. <br>You can check the project out <a href ="https://github.com/JackNulty/ClowningAround"> here! </a> </section>'
+
+        for (i = 0; i < allProj.length; i++) {  //hides all other project cards
+            console.log("iterating");
+
+            projName = allProj[i].getAttribute("id");
+
+            if (projName.includes("GGJ24")){  
+                //do nothing
+            }
+            else {
+                
+                allProj[i].className = 'project project-card-hidden';
+               
+            }
+        } 
 
     }
+    else if (projectCard.includes("GGJ23")) {
+
+    }
+
+
+
     //listen for click anywhere outside the expanded project card - elements outside it
     mainInfo.addEventListener("click",minimizeProject);
     buttonsSection.addEventListener("click",minimizeProject);
@@ -97,7 +120,15 @@ function expandProject(projectCard) {
 function minimizeProject() {
     console.log("minimizing project")
     //return the project to its menu form
-    currentProject.className = 'project project-card-visible'
+
+    for (i = 0; i < allProj.length; i++) {  //resets all projects to default
+        console.log("iterating");
+
+       allProj[i].className = 'project project-card-visible'
+    } 
+
+    currentProject.innerHTML = storedHTML;
+
     mainInfo.removeEventListener("click",minimizeProject); 
     buttonsSection.removeEventListener("click",minimizeProject);
 
